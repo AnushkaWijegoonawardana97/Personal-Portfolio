33b3ef0ceb95686c4f22893c663835eb17481b4d
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2021-01-29 07:42:48 +0530

    New dashboard development started fro the prrsonal protfolio

---
+++ Website Development/admin-dashboard/src/containers/NavigationBar/NavigationBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavigationBar() {
+	return (
+		<nav className="navbar navbar-dark bg-dark">
+			<div className="container-fluid">
+				<a href="#" className="navbar-brand"></a>
+			</div>
+		</nav>
+	);
+}
+
+export default NavigationBar;
